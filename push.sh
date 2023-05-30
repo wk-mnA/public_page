a35bd16cc7d9d46ef124e8395f13807ae66d9cb5
wkurata <wakako.kurata.px@renesas.com>
2023-05-30 19:26:49 +0900

    1.00

--- push.sh
+++ push.sh
@@ -6,6 +6,10 @@ if [$VERSION = ""]; then
   exit 1
 fi
 
+cd docs
+bundle exec jekyll build
+cd ..
+
 # git checkout origin/master
 sed -i -e  "s/RELEASE_VERSION=[0-9]*\.[0-9][0-9]/RELEASE_VERSION=${VERSION}/g" ./.github/workflows/deploy-version.yml 
 git add .
