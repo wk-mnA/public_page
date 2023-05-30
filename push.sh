f7cb1c35dfc5621b5881db45068a66a9f0d89c72
wk-mnA <wk-mnA@users.noreply.github.com>
2023-05-30 08:23:32 +0000

    deploy: a43a67c5401bc9cf496410767742019a94324288

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=$1
-
-# git checkout origin/master
-sed -i -e  "s/RELEASE_VERSION=[0-9]*\.[0-9][0-9]/RELEASE_VERSION=${VERSION}/g" ./.github/workflows/deploy-version.yml 
-git add .
-git commit -m "${VERSION}"
-git push origin master
