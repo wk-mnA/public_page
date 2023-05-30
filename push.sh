533e0e4d068d360baf3a6a297235d03c3b256b1e
wkurata <wakako.kurata.px@renesas.com>
2023-05-30 19:11:25 +0900

    1.00

--- push.sh
+++ push.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 VERSION=$1
+if [$VERSION = ""]; then
+  echo "ERROR: please type version. e.g. ./push.sh 1.00"
+  exit 1
+fi
 
 # git checkout origin/master
 sed -i -e  "s/RELEASE_VERSION=[0-9]*\.[0-9][0-9]/RELEASE_VERSION=${VERSION}/g" ./.github/workflows/deploy-version.yml 
