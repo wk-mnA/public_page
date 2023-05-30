8ed85deb6a2647c4ce49b0615fdfbb4c86d9d54e
wkurata <wakako.kurata.px@renesas.com>
2023-05-30 17:49:44 +0900

    redirect latest to new version 1.00

--- redirect.sh
+++ redirect.sh
@@ -11,6 +11,8 @@ ln -sf $VERSION latest
 # git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 # git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 cp $VERSION/_config.yml .
+cp $VERSION/Gemfile .
+cp $VERSION/Gemfile.lock .
 git add .
 git commit -m "redirect latest to new version $VERSION"
 # git pull origin origin/gh-pages
